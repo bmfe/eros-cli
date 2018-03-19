c7129d4506febcb8b3d8e57c7f5645ed88f8f076
Mingzhe Yang <yangmingzhe@benmu-health.com>
2018-03-19 18:04:05 +0800

    [feature] add hotrefresh

--- utils/readConfig.js
+++ utils/readConfig.js
@@ -25,6 +25,7 @@ function readAllConfig() {
         CONFIG = _.assign({
             weex: true,
             appName: erosConfig.appName,
+            hotRefresh: erosConfig.hotRefresh,
             appBoard: erosConfig.appBoard,
             localZipFolder: erosConfig.zipFolder,
             version: erosConfig.version,
